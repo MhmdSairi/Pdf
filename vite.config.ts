81bfcc2474ba3f50825fe10fbaf0320e967bdab3
Renier <renierr@users.noreply.github.com>
2025-11-06 21:49:32 +0100

    add config to reload service worker

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,8 @@ export default defineConfig(({ mode }) => ({
       includeAssets: ['favicon.ico', 'images/favicon.png', 'images/favicon.svg', 'qpdf.wasm'],
       manifest: require('./public/manifest.json'),
       workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2,woff,json,wasm}'],
         navigateFallback: '/index.html',
         maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 5MB for large JS bundles
